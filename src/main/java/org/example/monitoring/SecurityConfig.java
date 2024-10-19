12d881dbf2ae6be5ab3b92c4e9bf7c5520b5985c
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-10-20 00:58:06 +0200

    modificar dependencias para q funcione el springframework boot y el security

--- src/main/java/org/example/monitoring/SecurityConfig.java
+++ src/main/java/org/example/monitoring/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
